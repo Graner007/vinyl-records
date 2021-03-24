4f7763bbc0692e8e899ee03911231eb329dad449
Graner007 <graner.bence007@gmail.com>
2021-03-24 12:24:55 +0100

    fix eventlisteners

--- src/main/webapp/static/js/addToCart.js
+++ src/main/webapp/static/js/addToCart.js
@@ -1,27 +1,45 @@
-const buttons = document.querySelectorAll(".card-text");
 let productsNumber = document.querySelector("#products-number");
 const shoppingCart = document.querySelector("#cart");
+const names = document.querySelectorAll(".card-title");
+const prices = document.querySelectorAll(".lead");
 
 shoppingCart.addEventListener("click", () => {
     window.location.href = '/shopping-cart';
 });
 
-for (let button of buttons) {
-    button.addEventListener('click', () => {
-        let data = {
-            name: button.parentNode.previousSibling.previousSibling.firstChild.nextSibling.textContent,
-            price: button.parentNode.firstChild.nextSibling.firstChild.nextSibling.textContent
-        };
-        fetch('/', {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(data)
-        })
-            .then(response => response.json())
-            .then(async data => {
-                productsNumber.innerHTML = await data;
-            })
-            .catch((error) => {console.error("Error: " + error);
-            });
+function addGlobalEventListener(type, classname, callback) {
+    document.addEventListener(type, e => {
+        if (e.target.className == classname) callback (e)
     });
 }
+
+addGlobalEventListener("click", "btn btn-outline-primary", e => {
+    let name;
+    let price;
+
+    for (let i = 0; i < names.length; i++) {
+        if (names[i].textContent == e.target.id) {
+            name = e.target.id;
+            price = prices[i].textContent;
+        }
+    }
+
+    let data = {
+        name: name,
+        price: price
+    };
+
+    console.log(data);
+
+    fetch('/', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data)
+    })
+        .then(response => response.json())
+        .then(async data => {
+            productsNumber.innerHTML = await data;
+        })
+        .catch((error) => {console.error("Error: " + error);
+        });
+});
