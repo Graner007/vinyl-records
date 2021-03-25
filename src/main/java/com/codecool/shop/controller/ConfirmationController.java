17f8a0ba0eb7a332d344f8375dae3ec49d60c726
Mark Maczak <markmaczak@gmail.com>
2021-03-25 12:01:11 +0100

    Update ConfirmationController.java

--- src/main/java/com/codecool/shop/controller/ConfirmationController.java
+++ src/main/java/com/codecool/shop/controller/ConfirmationController.java
@@ -1,9 +1,18 @@
 package com.codecool.shop.controller;
 
-import com.codecool.shop.dao.*;
-import com.codecool.shop.dao.implementation.*;
 import com.codecool.shop.config.TemplateEngineUtil;
-import com.codecool.shop.model.*;
+import com.codecool.shop.dao.GenreDao;
+import com.codecool.shop.dao.LineItemDao;
+import com.codecool.shop.dao.OrderDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.implementation.GenreDaoMem;
+import com.codecool.shop.dao.implementation.LineItemDaoMem;
+import com.codecool.shop.dao.implementation.OrderDaoMem;
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.model.Genre;
+import com.codecool.shop.model.LineItem;
+import com.codecool.shop.model.Order;
+import com.codecool.shop.model.Payment;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import org.thymeleaf.TemplateEngine;
@@ -21,89 +30,16 @@ import java.util.List;
 @WebServlet(urlPatterns = {"/confirmation"})
 public class ConfirmationController extends HttpServlet {
 
-    private ProductDao productDataStore = ProductDaoMem.getInstance();
-    private GenreDao productCategoryDataStore = GenreDaoMem.getInstance();
     private OrderDao orderDataStore = OrderDaoMem.getInstance();
-    private ArtistDao artistDataStore = ArtistDaoMem.getInstance();
-    private LineItemDao lineItemDataStore = LineItemDaoMem.getInstance();
-
-    private List<Product> products = productDataStore.getAll();
-    private List<Artist> artists = artistDataStore.getAll();
-    private List<Genre> genres = productCategoryDataStore.getAll();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
 
-        context.setVariable("genres", genres);
-        context.setVariable("products", products);
-
         engine.process("product/confirmation.html", context, resp.getWriter());
-    }
+        orderDataStore.find(1).getUser();
+        orderDataStore.find(1).getProducts();
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        JsonObject data = new Gson().fromJson(req.getReader(), JsonObject.class);
-
-        resp.setContentType("application/json");
-        resp.setCharacterEncoding("UTF-8");
-
-        if (data.get("name") != null && data.get("price") != null) {
-            String recordName = data.get("name").getAsString();
-            float recordPrice = Float.parseFloat(data.get("price").getAsString().split(" ")[0]);
-
-            LineItem lineItem = new LineItem(recordName,1, recordPrice);
-            lineItemDataStore.add(lineItem);
-
-            Order order = orderDataStore.find(1);
-            order.addProduct(lineItem);
-
-            resp.getWriter().write(new Gson().toJson(order.getProductNumbers()));
-        }
-        else if (data.get("text") != null) {
-            String text = data.get("text").getAsString();
-            List<String> names = new ArrayList<>();
-
-            switch (text) {
-                case "genre":
-                    genres.forEach(genre -> names.add(genre.getName()));
-                    resp.getWriter().write(new Gson().toJson(names));
-                    break;
-                case "artist":
-                    artists.forEach(artist -> names.add(artist.getName()));
-                    resp.getWriter().write(new Gson().toJson(names));
-                    break;
-            }
-        }
-        else if (data.get("filter") != null) {
-            String filter = data.get("filter").getAsString();
-            List<List<String>> filterdProducts = new ArrayList<>();
-            List<Product> newProducts = new ArrayList<>();
-
-            products.forEach(product-> {
-                List<String> productList = new ArrayList<>();
-                if (product.getProductCategory().getName().equals(filter)) {
-                    productList.add(product.getName());
-                    productList.add(product.getProductCategory().getName());
-                    productList.add(product.getPrice());
-                    productList.add(product.getDescription());
-                    filterdProducts.add(productList);
-                    newProducts.add(product);
-                }
-                if (product.getSupplier().getName().equals(filter)) {
-                    productList.add(product.getName());
-                    productList.add(product.getProductCategory().getName());
-                    productList.add(product.getPrice());
-                    productList.add(product.getDescription());
-                    filterdProducts.add(productList);
-                    newProducts.add(product);
-                }
-            });
-
-            products = newProducts;
-
-            resp.getWriter().write(new Gson().toJson(filterdProducts));
-        }
     }
 }
